10e9ccc0bff0705861fc17ae409ae9e4fa705209
wessam18 <wessamabdelnabi903@gmail.com>
2024-12-24 18:02:24 +0200

    backend

--- dist/movies/omdb.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class OmdbService {
-    private readonly httpService;
-    private readonly API_KEY;
-    private readonly BASE_URL;
-    constructor(httpService: HttpService);
-    searchMovies(query: string): Promise<any>;
-}
--- prisma/migrations/20241224131230_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `director` to the `FavoriteMovie` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "FavoriteMovie" ADD COLUMN     "director" TEXT NOT NULL;
